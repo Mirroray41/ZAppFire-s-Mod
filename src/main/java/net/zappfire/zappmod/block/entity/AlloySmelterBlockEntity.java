cb1f3c8d6b2e69b9c709cd834eb8a37593ac08eb
ZAppFireCZ <87991171+ZAppFireCZ@users.noreply.github.com>
2022-01-17 22:00:37 +0100

    Alloy Smelter gui 70% done

--- src/main/java/net/zappfire/zappmod/block/entity/AlloySmelterBlockEntity.java
+++ src/main/java/net/zappfire/zappmod/block/entity/AlloySmelterBlockEntity.java
@@ -2,18 +2,24 @@ package net.zappfire.zappmod.block.entity;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventories;
+import net.minecraft.inventory.SimpleInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandler;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.zappfire.zappmod.block.custom.AlloySmelter;
+import net.zappfire.zappmod.item.ModItems;
 import net.zappfire.zappmod.item.inventory.ImplementedInventory;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,7 +27,7 @@ public class AlloySmelterBlockEntity extends BlockEntity implements NamedScreenH
 
     private final DefaultedList<ItemStack> inventory = DefaultedList.ofSize(3, ItemStack.EMPTY);
 
-    public AlloySmelterBlockEntity(, BlockPos pos, BlockState state) {
+    public AlloySmelterBlockEntity(BlockPos pos, BlockState state) {
         super(ModBlockEntities.ALLOY_SMELTER_BLOCK_ENTITY, pos, state);
     }
 
@@ -47,8 +53,36 @@ public class AlloySmelterBlockEntity extends BlockEntity implements NamedScreenH
     }
 
     @Override
-    public NbtCompound writeNbt(NbtCompound nbt) {
+    public void writeNbt(NbtCompound nbt) {
         Inventories.writeNbt(nbt, inventory);
-        return super.writeNbt(nbt);
+        super.writeNbt(nbt);
+    }
+    public static void tick(World world, BlockPos pos, BlockState state, AlloySmelterBlockEntity entity) {
+        if(hasRecipe(entity) && world.isThundering() && hasNotReachedStackLimit(entity)) {
+            craftItem(entity);
+
+            if(!world.isClient()) {
+                EntityType.LIGHTNING_BOLT.spawn((ServerWorld) world, null, null, null, pos,
+                        SpawnReason.TRIGGERED, true, true);
+            }
+        }
+    }
+
+    private static void craftItem(AlloySmelterBlockEntity entity) {
+        entity.removeStack(0, 1);
+        entity.removeStack(1, 1);
+
+        entity.setStack(2, new ItemStack(ModItems.SAPPHIRE, entity.getStack(2).getCount() + 1));
+    }
+
+    private static boolean hasRecipe(LightningChannelerBlockEntity entity) {
+        boolean hasItemInFirstSlot = entity.getStack(0).getItem() == ModItems.IRON_WOOL;
+        boolean hasItemInSecondSlot = entity.getStack(1).getItem() == ModItems.PEPPER;
+
+        return hasItemInFirstSlot && hasItemInSecondSlot;
+    }
+
+    private static boolean hasNotReachedStackLimit(LightningChannelerBlockEntity entity) {
+        return entity.getStack(2).getCount() < entity.getStack(2).getMaxCount();
     }
 }
